20ca5ff3182e5f2ddecf19362864baac2a87400b
alvinlal <alvinzzz2001@gmail.com>
2023-02-22 17:24:23 +0530

    deploy test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello from ECS.';
+    return 'Hello from ECS, how are you ?';
   }
 }
